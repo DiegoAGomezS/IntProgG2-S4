863ad69034a8cc5837832704f76ae648f5289234
DiegoAGomezS <dagomez@uamv.edu.ni>
2025-04-08 08:41:39 -0600

    Variables y tipos de datos

---
+++ ejercicio1.py
@@ -0,0 +1,14 @@
+#declarar variables
+nombre_completo = "Jose Cristo Carvallo Herrera"
+edad = 17
+altura = 1.72
+soltero = True
+
+print(f"Nombre completo: {nombre_completo}")
+print(f"Tipo de dato: {type(nombre_completo)}")
+print(f"Nombre completo: {edad}")
+print(f"Tipo de dato: {type(edad)}")
+print(f"Nombre completo: {altura:.2f}")
+print(f"Tipo de dato: {type(altura)}")
+print(f"Nombre completo: {soltero}")
+print(f"Tipo de dato: {type(soltero)}")
