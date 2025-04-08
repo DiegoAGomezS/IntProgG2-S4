570d49bbc6de531c7b4bda9fde96416a39b0f03d
DiegoAGomezS <dagomez@uamv.edu.ni>
2025-04-08 11:22:28 -0600

    Octavo ejercicio/primer ejercicio del inciso 2

---
+++ EjerciciosEvaluativos/Ejercicio8.py
@@ -0,0 +1,20 @@
+variable1 = "totalAmount"
+convencion1 = "camelCase"
+
+variable2 = "TotalAmount"
+convencion2 = "PascalCase"
+
+variable3 = "total_amount"
+convencion3 = "snake_case"
+
+variable4 = "TOTAL_AMOUNT"
+convencion4 = "SCREAMING_SNAKE_CASE"
+
+variable5 = "total-amount"
+convencion5 = "kebab-case"
+
+print(variable1, "->", convencion1)
+print(variable2, "->", convencion2)
+print(variable3, "->", convencion3)
+print(variable4, "->", convencion4)
+print(variable5, "->", convencion5)
