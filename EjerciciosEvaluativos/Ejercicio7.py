2c4d9c9db511cb4f834ea8e5178aecd9f0647913
DiegoAGomezS <dagomez@uamv.edu.ni>
2025-04-08 11:10:59 -0600

    Septimo ejercicio

---
+++ EjerciciosEvaluativos/Ejercicio7.py
@@ -0,0 +1,14 @@
+a = 17
+b = 23
+
+print("\nValores originales de las variables")
+print("a =", a)
+print("b =", b)
+
+c = a
+a = b
+b = c
+
+print("\nValores intercambiados")
+print("a =", a)
+print("b =", b)
