33e4b2a5dcedea5dea09d06a68dd96dc26f4e123
DiegoAGomezS <dagomez@uamv.edu.ni>
2025-04-08 10:45:42 -0600

    Tercer ejercicio

---
+++ EjerciciosEvaluativos/Ejercicio3.py
@@ -0,0 +1,6 @@
+primer_nombre = input("Ingrese su primer nombre: ")
+primer_apellido = input("Ingrese su primer apellido: ")
+
+nombre_completo = (primer_nombre + " " + primer_apellido)
+
+print(nombre_completo)
