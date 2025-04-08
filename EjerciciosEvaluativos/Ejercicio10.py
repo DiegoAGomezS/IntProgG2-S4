b65071255fcbb15ae91e70cc2a6cfee04ae7369c
DiegoAGomezS <dagomez@uamv.edu.ni>
2025-04-08 11:40:24 -0600

    Ejercicios 10 y 11

---
+++ EjerciciosEvaluativos/Ejercicio10.py
@@ -0,0 +1,14 @@
+print ("""Declara nombres de variables para los siguientes casos, aplicando la convención
+indicada.""")
+
+a = "Nombre completo del estudiante → camelCase"
+b = "Número de matrícula → snake_case"
+c = "Total de ventas del mes → PascalCase"
+d = "Límite máximo de intentos → SCREAMING_SNAKE_CASE"
+e = "Cantidad de productos → camelCase"
+
+print(f"{a} → nombreCompletoEstudiante")
+print(f"{b} → numero_matricula")
+print(f"{c} → totalDeVentasDelMes")
+print(f"{d} → LIMITE_MAXIMO_DE_INTENTOS")
+print(f"{e} → cantidadDeProductos")
---
+++ EjerciciosEvaluativos/Ejercicio11.py
@@ -0,0 +1,14 @@
+print ("""A partir de las siguientes descripciones, escribe el nombre de una variable que siga
+una convención lógica y clara. Puedes elegir la convención, pero justificarla.""")
+
+a = "La edad del usuario"
+b = "El precio total del carrito de compras"
+c = "El estado de inicio de sesión (activo o inactivo)"
+d = "El nombre del curso actual"
+e = "La temperatura promedio registrada"
+
+print(f"{a} → edad_usuario")
+print(f"{b} → totalCarritoCompras")
+print(f"{c} → incicio_sesion")
+print(f"{d} → cursoActual")
+print(f"{e} → temp_registrada")
